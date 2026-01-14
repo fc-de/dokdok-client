76b60743682139fd2cc430d23e15ff96a763422d
mgYang53 <mg960503@gmail.com>
2026-01-14 23:05:42 +0900

    chore: lint 설정 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import tanstackQuery from '@tanstack/eslint-plugin-query'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,10 +17,59 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      tanstackQuery.configs['flat/recommended'],
+      eslintConfigPrettier,
     ],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      '@typescript-eslint/naming-convention': [
+        'error',
+        // 기본: camelCase
+        {
+          selector: 'default',
+          format: ['camelCase'],
+        },
+        // 변수: camelCase, UPPER_CASE (상수), PascalCase (컴포넌트)
+        {
+          selector: 'variable',
+          format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+        },
+        // 함수: camelCase, PascalCase (컴포넌트)
+        {
+          selector: 'function',
+          format: ['camelCase', 'PascalCase'],
+        },
+        // 타입/인터페이스: PascalCase
+        {
+          selector: 'typeLike',
+          format: ['PascalCase'],
+        },
+        // import: 모든 형식 허용
+        {
+          selector: 'import',
+          format: null,
+        },
+        // 객체 속성: 외부 API 응답 등을 위해 유연하게
+        {
+          selector: 'objectLiteralProperty',
+          format: null,
+        },
+        // 구조 분해 변수: 외부 라이브러리 호환
+        {
+          selector: 'variable',
+          modifiers: ['destructured'],
+          format: null,
+        },
+      ],
     },
   },
 ])
