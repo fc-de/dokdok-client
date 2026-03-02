af3d7fe3c45ba9af826a5e836c280360570f68ab
Choi Youngae <choiaeyoung1001@gmail.com>
2026-03-02 21:30:48 +0900

    refactor: RootLayout에서 불필요한 overflow-x-clip 제거 (#59)

--- src/shared/layout/RootLayout.tsx
+++ src/shared/layout/RootLayout.tsx
@@ -8,7 +8,7 @@ import { Outlet } from 'react-router-dom'
  */
 export default function RootLayout() {
   return (
-    <div className="min-h-screen bg-white overflow-x-clip">
+    <div className="min-h-screen bg-white">
       <Outlet />
     </div>
   )
