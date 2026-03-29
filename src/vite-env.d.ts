9b578650f4035b44d2761611322f0418a4ca7120
mgYang53 <mg960503@gmail.com>
2026-03-29 23:35:20 +0900

    chore: 환경 변수 타입 선언 추가 (#109)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_APP_URL: string
   readonly VITE_KAKAO_MAP_KEY: string
   readonly VITE_USE_MOCK?: string
+  readonly VITE_ENABLE_INTERNAL_LOGIN?: string
 }
 
 interface ImportMeta {
