1103c175d06e1d61934cc55aff932d6a89fc66f0
Choi Youngae <choiaeyoung1001@gmail.com>
2026-06-22 01:53:21 +0900

    design: 개인회고 글자수 카운터 추가 (#145)

--- src/features/retrospectives/personal/personalRetrospective.constants.ts
+++ src/features/retrospectives/personal/personalRetrospective.constants.ts
@@ -1,3 +1,19 @@
+export const CHANGED_THOUGHTS_LIMITS = {
+  CORE_ISSUE_MAX: 200,
+  POST_OPINION_MAX: 10000,
+} as const
+
+export const OTHERS_PERSPECTIVE_LIMITS = {
+  OPINION_MAX: 10_000,
+  IMPRESSIVE_REASON_MAX: 5000,
+} as const
+
+export const FREE_RECORD_LIMITS = {
+  LIST_MAX: 10,
+  TITLE_MAX: 40,
+  CONTENT_MAX: 100000,
+} as const
+
 export const PERSONAL_RETRO_SECTION_IDS = {
   changedThoughts: 'changed-thoughts',
   othersPerspective: 'others-perspective',
