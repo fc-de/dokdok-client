22d22138283f428a0c4bef375c96096dc30f526a
Haeun <haruyam15@gmail.com>
2026-07-15 20:01:17 +0900

    design: DatePicker 및 Calendar 컴포넌트 모바일 반응형 적용 (#162)

--- src/shared/ui/Calendar.tsx
+++ src/shared/ui/Calendar.tsx
@@ -6,7 +6,7 @@ import { type DayButton, DayPicker, getDefaultClassNames } from 'react-day-picke
 
 import { cn } from '@/shared/lib/utils'
 
-const calendarButtonBaseClass = `inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-small typo-caption1 transition-all disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none aria-invalid:border-destructive`
+const calendarButtonBaseClass = `inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-small typo-caption1 max-lg:typo-m-subtitle2 transition-all disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none aria-invalid:border-destructive`
 
 function Calendar({
   className,
@@ -20,132 +20,141 @@ function Calendar({
   const defaultClassNames = getDefaultClassNames()
 
   return (
-    <DayPicker
-      locale={ko}
-      showOutsideDays={showOutsideDays}
-      disabled={{ before: startOfToday() }}
-      className={cn(
-        'bg-white group/calendar [--cell-size:--spacing(8)] pt-medium px-medium pb-base rounded-base in-data-[slot=card-content]:bg-transparent in-data-[slot=popover-content]:bg-transparent',
-        String.raw`rtl:**:[.rdp-button\_next>svg]:rotate-180`,
-        String.raw`rtl:**:[.rdp-button\_previous>svg]:rotate-180`,
-        className
-      )}
-      modifiers={{
-        dayOfWeek: { dayOfWeek: [0] },
-      }}
-      modifiersClassNames={{
-        dayOfWeek: 'is-weekend',
-      }}
-      captionLayout={captionLayout}
-      formatters={{
-        formatMonthDropdown: (date) => date.toLocaleString('ko-KR', { month: 'long' }),
-        formatYearDropdown: (date) => `${date.getFullYear()}년`,
-        formatCaption: (date) =>
-          `${date.getFullYear()}년 ${String(date.getMonth() + 1).padStart(2, '0')}월`,
-        ...formatters,
-      }}
-      classNames={{
-        root: cn('w-fit', defaultClassNames.root),
-        months: cn('flex gap-4 flex-col md:flex-row relative', defaultClassNames.months),
-        month: cn('flex flex-col w-full gap-4', defaultClassNames.month),
-        nav: cn(
-          'flex items-center gap-1 w-full absolute top-0 inset-x-0 justify-between',
-          defaultClassNames.nav
-        ),
-        button_previous: cn(
-          calendarButtonBaseClass,
-          'size-(--cell-size) aria-disabled:opacity-30 p-0 select-none hover:bg-grey-200 ',
-          defaultClassNames.button_previous
-        ),
-        button_next: cn(
-          calendarButtonBaseClass,
-          'size-(--cell-size) aria-disabled:opacity-30 p-0 select-none hover:bg-grey-200',
-          defaultClassNames.button_next
-        ),
-        month_caption: cn(
-          'flex items-center justify-center h-(--cell-size) w-full px-(--cell-size) typo-body3 text-black',
-          defaultClassNames.month_caption
-        ),
-        dropdowns: cn(
-          'w-full flex items-center text-sm font-medium justify-center h-(--cell-size) gap-1.5',
-          defaultClassNames.dropdowns
-        ),
-        dropdown_root: cn(
-          'relative has-focus:border-ring border border-input shadow-xs has-focus:ring-ring/50 has-focus:ring-[3px] rounded-md',
-          defaultClassNames.dropdown_root
-        ),
-        dropdown: cn('absolute bg-popover inset-0 opacity-0', defaultClassNames.dropdown),
-        caption_label: cn(
-          'select-none',
-          captionLayout === 'label'
-            ? 'typo-body3'
-            : 'rounded-md pl-2 pr-1 flex items-center gap-1 text-sm h-8 [&>svg]:text-muted-foreground [&>svg]:size-3.5',
-          defaultClassNames.caption_label
-        ),
-        table: 'w-full border-collapse',
-        weekdays: cn('flex', defaultClassNames.weekdays),
-        weekday: cn(
-          'text-grey-600 flex-1 rounded-small select-none typo-caption1',
-          defaultClassNames.weekday
-        ),
-        week: cn('flex w-full mt-2', defaultClassNames.week),
-        week_number_header: cn('select-none w-(--cell-size)', defaultClassNames.week_number_header),
-        week_number: cn('text-[0.8rem] select-none text-grey-600', defaultClassNames.week_number),
-        day: cn(
-          'relative w-full h-full p-0 text-center group/day select-none [&:last-child[data-selected=true]_button]:rounded-r-small aspect-square',
-          props.showWeekNumber
-            ? '[&:nth-child(2)[data-selected=true]_button]:rounded-l-small'
-            : '[&:first-child[data-selected=true]_button]:rounded-l-small',
-          defaultClassNames.day
-        ),
-        range_start: cn('rounded-l-small bg-grey-200', defaultClassNames.range_start),
-        range_middle: cn('rounded-none', defaultClassNames.range_middle),
-        range_end: cn('rounded-r-small bg-grey-200', defaultClassNames.range_end),
-        today: cn(
-          'bg-transparent text-black font-semibold rounded-small data-[selected=true]:rounded-none',
-          defaultClassNames.today
-        ),
-        outside: cn(
-          'text-grey-500 aria-selected:text-grey-500 opacity-50',
-          defaultClassNames.outside
-        ),
-        disabled: cn('text-grey-400', defaultClassNames.disabled),
-        hidden: cn('invisible', defaultClassNames.hidden),
-        ...classNames,
-      }}
-      components={{
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        Root: ({ className, rootRef, ...props }) => {
-          return <div data-slot="calendar" ref={rootRef} className={cn(className)} {...props} />
-        },
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        Chevron: ({ className, orientation, ...props }) => {
-          if (orientation === 'left') {
-            return <ChevronLeftIcon className={cn('size-4 text-grey-600', className)} {...props} />
-          }
+    <div>
+      <DayPicker
+        locale={ko}
+        showOutsideDays={showOutsideDays}
+        disabled={{ before: startOfToday() }}
+        className={cn(
+          'bg-white group/calendar [--cell-size:--spacing(8)] max-lg:[--cell-size:--spacing(12)] pt-medium px-medium pb-base rounded-base in-data-[slot=card-content]:bg-transparent in-data-[slot=popover-content]:bg-transparent',
+          String.raw`rtl:**:[.rdp-button\_next>svg]:rotate-180`,
+          String.raw`rtl:**:[.rdp-button\_previous>svg]:rotate-180`,
+          className
+        )}
+        modifiers={{
+          dayOfWeek: { dayOfWeek: [0] },
+        }}
+        modifiersClassNames={{
+          dayOfWeek: 'is-weekend',
+        }}
+        captionLayout={captionLayout}
+        formatters={{
+          formatMonthDropdown: (date) => date.toLocaleString('ko-KR', { month: 'long' }),
+          formatYearDropdown: (date) => `${date.getFullYear()}년`,
+          formatCaption: (date) =>
+            `${date.getFullYear()}년 ${String(date.getMonth() + 1).padStart(2, '0')}월`,
+          ...formatters,
+        }}
+        classNames={{
+          root: cn('w-fit', defaultClassNames.root),
+          months: cn('flex gap-4 flex-col md:flex-row relative', defaultClassNames.months),
+          month: cn('flex flex-col w-full gap-4', defaultClassNames.month),
+          nav: cn(
+            'flex items-center gap-1 w-full absolute top-0 inset-x-0 justify-between',
+            defaultClassNames.nav
+          ),
+          button_previous: cn(
+            calendarButtonBaseClass,
+            'size-(--cell-size) aria-disabled:opacity-30 p-0 select-none hover:bg-grey-200 ',
+            defaultClassNames.button_previous
+          ),
+          button_next: cn(
+            calendarButtonBaseClass,
+            'size-(--cell-size) aria-disabled:opacity-30 p-0 select-none hover:bg-grey-200',
+            defaultClassNames.button_next
+          ),
+          month_caption: cn(
+            'flex items-center justify-center h-(--cell-size) w-full px-(--cell-size) max-lg:typo-m-subtitle2 typo-body3 text-black',
+            defaultClassNames.month_caption
+          ),
+          dropdowns: cn(
+            'w-full flex items-center text-sm font-medium justify-center h-(--cell-size) gap-1.5',
+            defaultClassNames.dropdowns
+          ),
+          dropdown_root: cn(
+            'relative has-focus:border-ring border border-input shadow-xs has-focus:ring-ring/50 has-focus:ring-[3px] rounded-md',
+            defaultClassNames.dropdown_root
+          ),
+          dropdown: cn('absolute bg-popover inset-0 opacity-0', defaultClassNames.dropdown),
+          caption_label: cn(
+            'select-none',
+            captionLayout === 'label'
+              ? 'typo-body3 max-lg:typo-m-subtitle2'
+              : 'rounded-md pl-2 pr-1 flex items-center gap-1 text-sm h-8 [&>svg]:text-muted-foreground [&>svg]:size-3.5',
+            defaultClassNames.caption_label
+          ),
+          table: 'w-full border-collapse',
+          weekdays: cn('flex', defaultClassNames.weekdays),
+          weekday: cn(
+            'text-grey-600 flex-1 rounded-small select-none typo-caption1',
+            defaultClassNames.weekday
+          ),
+          week: cn('flex w-full mt-2', defaultClassNames.week),
+          week_number_header: cn(
+            'select-none w-(--cell-size)',
+            defaultClassNames.week_number_header
+          ),
+          week_number: cn('text-[0.8rem] select-none text-grey-600', defaultClassNames.week_number),
+          day: cn(
+            'relative w-full h-full p-0 text-center group/day select-none [&:last-child[data-selected=true]_button]:rounded-r-small aspect-square',
+            props.showWeekNumber
+              ? '[&:nth-child(2)[data-selected=true]_button]:rounded-l-small'
+              : '[&:first-child[data-selected=true]_button]:rounded-l-small',
+            defaultClassNames.day
+          ),
+          range_start: cn('rounded-l-small bg-grey-200', defaultClassNames.range_start),
+          range_middle: cn('rounded-none', defaultClassNames.range_middle),
+          range_end: cn('rounded-r-small bg-grey-200', defaultClassNames.range_end),
+          today: cn(
+            'bg-transparent text-black font-semibold rounded-small data-[selected=true]:rounded-none',
+            defaultClassNames.today
+          ),
+          outside: cn(
+            'text-grey-500 aria-selected:text-grey-500 opacity-50',
+            defaultClassNames.outside
+          ),
+          disabled: cn('text-grey-400', defaultClassNames.disabled),
+          hidden: cn('invisible', defaultClassNames.hidden),
+          ...classNames,
+        }}
+        components={{
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          Root: ({ className, rootRef, ...props }) => {
+            return <div data-slot="calendar" ref={rootRef} className={cn(className)} {...props} />
+          },
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          Chevron: ({ className, orientation, ...props }) => {
+            if (orientation === 'left') {
+              return (
+                <ChevronLeftIcon className={cn('size-4 text-grey-600', className)} {...props} />
+              )
+            }
 
-          if (orientation === 'right') {
-            return <ChevronRightIcon className={cn('size-4 text-grey-600', className)} {...props} />
-          }
+            if (orientation === 'right') {
+              return (
+                <ChevronRightIcon className={cn('size-4 text-grey-600', className)} {...props} />
+              )
+            }
 
-          return <ChevronDownIcon className={cn('size-4', className)} {...props} />
-        },
+            return <ChevronDownIcon className={cn('size-4', className)} {...props} />
+          },
 
-        DayButton: CalendarDayButton,
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        WeekNumber: ({ children, ...props }) => {
-          return (
-            <td {...props}>
-              <div className="flex size-(--cell-size) items-center justify-center text-center">
-                {children}
-              </div>
-            </td>
-          )
-        },
-        ...components,
-      }}
-      {...props}
-    />
+          DayButton: CalendarDayButton,
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          WeekNumber: ({ children, ...props }) => {
+            return (
+              <td {...props}>
+                <div className="flex size-(--cell-size) items-center justify-center text-center">
+                  {children}
+                </div>
+              </td>
+            )
+          },
+          ...components,
+        }}
+        {...props}
+      />
+    </div>
   )
 }
 
--- src/shared/ui/Datepicker.tsx
+++ src/shared/ui/Datepicker.tsx
@@ -3,7 +3,17 @@ import { CalendarDays } from 'lucide-react'
 import * as React from 'react'
 import type { Matcher } from 'react-day-picker'
 
+import { useDevice } from '@/shared/hooks/useDevice'
 import { cn } from '@/shared/lib/utils'
+import {
+  BottomSheet,
+  BottomSheetClose,
+  BottomSheetContent,
+  BottomSheetHeader,
+  BottomSheetTitle,
+  BottomSheetTrigger,
+} from '@/shared/ui/BottomSheet'
+import { Button } from '@/shared/ui/Button'
 import { Calendar } from '@/shared/ui/Calendar'
 import { Popover, PopoverContent, PopoverTrigger } from '@/shared/ui/Popover'
 
@@ -35,6 +45,7 @@ const DatePicker = React.forwardRef<HTMLButtonElement, DatePickerProps>(function
   ref
 ) {
   const [date, setDate] = React.useState<Date | null>(value)
+  const { isMobile } = useDevice()
 
   React.useEffect(() => {
     setDate(value)
@@ -45,27 +56,56 @@ const DatePicker = React.forwardRef<HTMLButtonElement, DatePickerProps>(function
     onChange?.(selectedDate)
   }
 
+  const triggerButton = (
+    <button
+      ref={ref}
+      type="button"
+      data-empty={!date}
+      disabled={isDisabled}
+      className={cn(
+        'flex w-full md:max-w-[265px] justify-start max-lg:justify-between text-left rounded-small border border-grey-300 data-[state=open]:border-primary-300 [&_svg]:text-grey-600 data-[state=open]:[&_svg]:text-primary-300 px-medium py-xsmall max-lg:p-base gap-small typo-subtitle5 transition-colors text-black items-center  disabled:opacity-50 disabled:cursor-not-allowed',
+        className
+      )}
+    >
+      <CalendarDays size={20} className="max-lg:order-last" />
+      {date ? (
+        format(date, 'yyyy.MM.dd')
+      ) : (
+        <span className="typo-subtitle5 text-grey-600">{placeholder}</span>
+      )}
+    </button>
+  )
+
+  if (isMobile) {
+    return (
+      <BottomSheet>
+        <BottomSheetTrigger asChild>{triggerButton}</BottomSheetTrigger>
+        <BottomSheetContent className="items-center">
+          <BottomSheetHeader hideCloseButton className="w-full max-w-[375px]">
+            <BottomSheetTitle className="typo-m-body1 text-black">날짜 선택</BottomSheetTitle>
+          </BottomSheetHeader>
+          <Calendar
+            mode="single"
+            selected={date ?? undefined}
+            onSelect={handleSelect}
+            required
+            disabled={disabled}
+          />
+          <div className="px-medium py-small w-full max-w-[375px]">
+            <BottomSheetClose asChild>
+              <Button size="large" className="w-full">
+                확인
+              </Button>
+            </BottomSheetClose>
+          </div>
+        </BottomSheetContent>
+      </BottomSheet>
+    )
+  }
+
   return (
     <Popover>
-      <PopoverTrigger asChild>
-        <button
-          ref={ref}
-          type="button"
-          data-empty={!date}
-          disabled={isDisabled}
-          className={cn(
-            'flex w-full md:max-w-[265px] justify-start text-left rounded-small border border-grey-300 data-[state=open]:border-primary-300 [&_svg]:text-grey-600 data-[state=open]:[&_svg]:text-primary-300 px-medium py-xsmall gap-small typo-subtitle5 transition-colors text-black items-center  disabled:opacity-50 disabled:cursor-not-allowed',
-            className
-          )}
-        >
-          <CalendarDays size={20} />
-          {date ? (
-            format(date, 'yyyy.MM.dd')
-          ) : (
-            <span className="typo-subtitle5 text-grey-600">{placeholder}</span>
-          )}
-        </button>
-      </PopoverTrigger>
+      <PopoverTrigger asChild>{triggerButton}</PopoverTrigger>
       <PopoverContent className="p-0 border-none w-fit" align="start">
         <Calendar
           mode="single"
