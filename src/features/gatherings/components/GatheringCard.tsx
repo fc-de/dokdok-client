52da95b63b24f42e5da06ea9f4c13e912f0306bd
mgYang53 <50770004+mgYang53@users.noreply.github.com>
2026-07-26 18:39:40 +0900

    design: 모임 목록 모바일 반응형 적용 (#186) (#187)

--- src/features/gatherings/components/GatheringCard.tsx
+++ src/features/gatherings/components/GatheringCard.tsx
@@ -9,12 +9,14 @@ interface GatheringCardProps {
   gathering: GatheringListItem
   onFavoriteToggle: (gatheringId: number) => void
   onClick: () => void
+  className?: string
 }
 
 export default function GatheringCard({
   gathering,
   onFavoriteToggle,
   onClick,
+  className,
 }: GatheringCardProps) {
   const {
     gatheringId,
@@ -41,7 +43,10 @@ export default function GatheringCard({
 
   return (
     <div
-      className="relative flex h-35 cursor-pointer flex-col justify-between rounded-base border border-grey-300 bg-white p-medium transition-colors hover:border-grey-400 max-lg:h-30 max-lg:p-4"
+      className={cn(
+        'relative flex h-35 cursor-pointer flex-col justify-between rounded-base border border-grey-300 bg-white p-medium transition-colors hover:border-grey-400 max-lg:h-30 max-lg:p-4',
+        className
+      )}
       onClick={onClick}
       role="button"
       tabIndex={0}
